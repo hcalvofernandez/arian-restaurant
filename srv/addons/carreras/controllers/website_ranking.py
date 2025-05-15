d0b1438e5cb53743f0cf12704704b5933d71b309
Hcalvofernandez <scnetisla@gmail.com>
2025-05-15 01:05:56 -0400

    eliminando modulos innecesarios

--- srv/addons/carreras/controllers/website_ranking.py
+++
@@ -1,7 +0,0 @@
-from odoo import http
-
-class WebsiteRanking(http.Controller):
-    @http.route('/ranking', type='http', auth='public', website=True)
-    def ranking(self):
-        partners = http.request.env['res.partner'].search([])
-        return http.request.render('carreras.ranking_template', {'partners': partners})
--- srv/addons/ciclistas/models/cycling_modality.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class CyclingModality(models.Model):
-    _name = 'cycling.modality'
-    _description = 'Cycling Modality'
-
-    name = fields.Char(string='Name', required=True)
-    description = fields.Text(string='Description')
